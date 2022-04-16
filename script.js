1c130b586578ec1205587ebdae334e20e9045f57
DESKTOP-NGEPSM5\BEST <larisaoprea333@yahoo.com>
2022-04-16 11:45:47 -0700

    almost done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let menub = document.querySelector('#menu-bar');
 let header = document.querySelector('.header');
+let jstxt = document.querySelector('.jstxt');
 
 menub.onclick = () =>{
     menub.classList.toggle('fa-times');
@@ -13,4 +14,11 @@ if(window.innerWidth <991){
     header.classList.remove('active');
     document.body.classList.remove('active');
 }
+if(window.innerWidth >991){
+    jstxt.innerHTML= "JavaScript";
+}
+if(window.innerWidth <991){
+    jstxt.innerHTML= "JS";
+}
+
 }
